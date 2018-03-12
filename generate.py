cd9d4267ec3014b968b15b526de75d7785b37dd6
artek.chumak <artek.chumak@gmail.com>
2018-03-12 18:02:15 +0300

    Add comments and docs. Code generator of text. Add text to train.

--- generate.py
+++ generate.py
@@ -1,16 +1,85 @@
+"""Генератор текста на основе биграмм. Для генерации текста надо использовать уже обученную модель(обучение идет из
+предлагаемых пользователем текстов). Для генерации текста можно указать параметры - начальное слово, длину. Вывод может
+происходить как и в stdout, так и в файл."""
 import argparse
-import pymorphy2
+import random
+import pickle
 
 
-def main(ns):
-    pass
+def load_model(dist_model):
+    """Load model from file"""
+    with open(dist_model, 'rb') as f:
+        return pickle.load(f)
+
+
+def weighted_choices(choices):
+    """Choise by weight"""
+    total = sum(w for c, w in choices)
+    # Choise float from 0 to total
+    r = random.uniform(0, total)
+    upto = 0
+    for c, w in choices:
+        if upto + w >= r:
+            return c
+        upto += w
+
+
+def generate_text(model, length, seed):
+    """Main function to generate text"""
+    t = []
+    if seed is None:
+        seed = random.choice(list(i[0] for i in model.keys()))
+    t.append(seed)
+    # Number of punctuation symbols
+    pnc = 0
+    while len(t) - pnc < length:
+        t.append(weighted_choices([(i[1], model[i]) for i in model if i[0] == t[-1]]))
+        if t[-1] in (',', '.', '!', '?', ';', ':'):
+            pnc += 1
+    return t
+
+
+def create_text(t):
+    """Create text from list of tokens"""
+    text = ''
+    for i in t:
+        if i in (',', '.', '!', '?', ';', ':'):
+            text += i
+        elif i == '$':
+            text += '\n'
+        else:
+            text += ' ' + i
+
+    # Adding last dot
+    if t[-1] not in ('.', '!', '?'):
+        text += '.'
+
+    return text
+
+
+def save_text(text, text_dest):
+    """Save text to file"""
+    with open(text_dest, 'w') as f:
+        f.write(text)
+
+
+def main(args):
+    """Main function"""
+    model = load_model(args['model'])
+    t = generate_text(model, args['length'], args['seed'])
+    text = create_text(t)
+    if args['output'] is None:
+        print(text)
+    else:
+        save_text(text, args['output'])
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate text using model.', prog='generate.py')
     parser.add_argument('model', action='store', help='destination to model file')
     parser.add_argument('--seed', action='store', help='set starting word')
-    parser.add_argument('length', action='store', help='length of generated text')
+    parser.add_argument('length', action='store', type=int, help='length of generated text')
     parser.add_argument('--output', action='store',
                         help='destination to output file. If it isn\'t set, uses stdout')
-    main(parser.parse_args())
+    args = vars(parser.parse_args())
+    main(args)
--- train.py
+++ train.py
@@ -1,15 +1,18 @@
+"""Программа для обучения модели программы генерации текстов. Реализация модели - через биграммы, ввод данных - через
+терминал(stdin) или через документы в папке. Модель сохраняется через модуль Pickle."""
+
 import argparse
 import os
 import re
 import pickle
 from collections import Counter
-#import pymorphy2
 
 
-r_alphabet = re.compile('[а-яА-Я0-9-{1}]+|[.,:;!?]+')
+r_alphabet = re.compile('[а-яА-Я0-9]+|[-.,:;!?]+')
 
 
 def gen_token(files):
+    """Generator to find all words in texts"""
     for file in files:
         for line in file:
             for token in r_alphabet.findall(line):
@@ -17,6 +20,7 @@ def gen_token(files):
 
 
 def gen_lines(file, lc):
+    """Generator to find all lines in the texts"""
     with open(file, 'r') as f:
         for line in f:
             if lc:
@@ -26,6 +30,7 @@ def gen_lines(file, lc):
 
 
 def gen_files(input_dir, lc):
+    """Generator to find files in a folder"""
     if input_dir is None:
         return [[input()]]
     else:
@@ -33,6 +38,7 @@ def gen_files(input_dir, lc):
 
 
 def gen_bigrams(token):
+    """Generator to create bigrams from words"""
     t0 = '$'
     for t1 in token:
         yield t0, t1
@@ -44,49 +50,59 @@ def gen_bigrams(token):
 
 
 def gen_model(bigrams):
+    """Count words to create bigrams model"""
     p_model = Counter(i for i in bigrams)
-    return {i[0]: {i[1]: p_model[(i[0], i[1])]} for i in p_model}
+    # Return dict from word to words(i[1]) with their frequences in texts(p_model)
+    return {(i[0], i[1]): p_model[(i[0], i[1])] for i in p_model}
 
 
 def update_model(model, bigrams):
+    """Update model, if it exists"""
     u_model = Counter(i for i in bigrams)
     for i in u_model:
-        if i[0] in model:
-            if i[1] in model[i[0]]:
-                model[i[0]][i[1]] += u_model[i]
-            else:
-                model[i[0]][i[1]] = u_model[i]
+        if i in model:
+            model[(i[0], i[1])] += u_model[i]
         else:
-            model[i[0]] = {i[1]: u_model[i]}
+            model[(i[0], i[1])] = u_model[i]
 
     return model
 
 
 def load_model(model_dest):
+    """Load model from file"""
     with open(model_dest, 'rb') as f:
         return pickle.load(f)
 
 
 def save_model(model, model_dest):
-    with open(model_dest, 'wb') as f:
+    """Save model to file"""
+    with open(model_dest + '.model', 'wb') as f:
         pickle.dump(model, f)
 
 
 def train_model(bigrams, model_dest):
+    """Main function for model"""
     if model_dest is None:
+        # create model, if it doesn't exist
         model = gen_model(bigrams)
-        model_dest = input("Write destination for model file:")
+        model_dest = input("Write destination for model file:\n")
         save_model(model, model_dest)
     else:
+        # train model, if it exists
         model = load_model(model_dest)
         model = update_model(model, bigrams)
         save_model(model, model_dest)
 
 
 def main(args):
+    """Main function to train model"""
+    # find files
     files = gen_files(args['input_dir'], args['lc'])
+    # find words in files
     token = gen_token(files)
+    # create bigrams
     bigrams = gen_bigrams(token)
+    # train or create model using bigrams
     train_model(bigrams, args['model'])
 
 

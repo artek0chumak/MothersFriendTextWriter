5e1e821521808b1d26acdcd292ee0405c1ff536b
artek.chumak <artek.chumak@gmail.com>
2018-03-19 10:45:28 +0300

    Первая рабочая версия.

--- generate.py
+++ generate.py
@@ -1,16 +1,124 @@
+"""
+Генератор текста из модели, полученной обучением с помощью train.py.
+Используйте -h для вызова справки.
+"""
 import argparse
-import pymorphy2
+import random
+import pickle
 
 
-def main(ns):
-    pass
+def load_model(dest_model):
+    """
+    Загрузка модели из файла
+    :param dest_model: Расположение файла модели
+    :type dest_model: str
+    :return: Словарь кортежа слов в частыт
+    :rtype: dict
+    """
+    with open(dest_model, 'rb') as f:
+        return pickle.load(f)
+
+
+def weighted_choices(choices):
+    """
+    Куммулятивное распределение
+    :param choices: Список слов и их частоты
+    :type choices: list
+    :return: Выбранное слово
+    :rtype: str
+    """
+    total = sum(w for c, w in choices)
+    r = random.uniform(0, total)
+    upto = 0
+    for c, w in choices:
+        if upto + w >= r:
+            return c
+        upto += w
+
+
+def generate_text(model, length, seed):
+    """
+    Генерация текста
+    :param model: Модель текстов
+    :param length: Длина текста в словах(знаки пунктуации считаются за слова)
+    :param seed: Начальное слово
+    :type model: dict
+    :type length: int
+    :type seed: str
+    :return: Список слов
+    :rtype: list
+    """
+    l_ngrams = len(next(iter(model)))
+    t = ['$' for _ in range(l_ngrams - 1)]
+    if seed is None:
+        seed = random.choice([i[-1] for i in model if list(i[:-1]) == t])
+    t.append(seed)
+    while len(t) < length + l_ngrams:
+        t.append(weighted_choices([(i[-1], model[i]) for i in model if list(i[:-1]) == t[-l_ngrams + 1:]]))
+        if t[-1] is None:
+            t[-1] = random.choice([i[-1] for i in model if list(i[:-1]) == ['$' for _ in range(l_ngrams - 1)]])
+
+    return t[l_ngrams - 1:]
+
+
+def create_text(t):
+    """
+    Создает текст из списка слов
+    :param t: Список слов
+    :type t: list
+    :return: Сгенерированный текст
+    :rtype: str
+    """
+    text = ''
+    for i in t:
+        if i in (',', '.', '!', '?', ';', ':'):
+            text += i
+        elif i == '$':
+            text += '\n'
+        else:
+            text += ' ' + i
+
+    if t[-1] not in ('.', '!', '?'):
+        text += '.'
+
+    return text
+
+
+def save_text(text, text_dest):
+    """
+    Сохраняет текст в файл
+    :param text: Текст
+    :param text_dest: Располжение файла текста
+    :type text: str
+    :type text_dest: str
+    :return: None
+    """
+    with open(text_dest, 'w') as f:
+        f.write(text)
+
+
+def main(args):
+    """
+    Главная функция
+    :param args: Аргументы запуска генератора
+    :type args: dict
+    :return: None
+    """
+    model = load_model(args['model'])
+    t = generate_text(model, args['length'], args['seed'])
+    text = create_text(t)
+    if args['output'] is None:
+        print(text)
+    else:
+        save_text(text, args['output'])
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate text using model.', prog='generate.py')
     parser.add_argument('model', action='store', help='destination to model file')
     parser.add_argument('--seed', action='store', help='set starting word')
-    parser.add_argument('length', action='store', help='length of generated text')
+    parser.add_argument('length', action='store', type=int, help='length of generated text')
     parser.add_argument('--output', action='store',
                         help='destination to output file. If it isn\'t set, uses stdout')
-    main(parser.parse_args())
+    args = vars(parser.parse_args())
+    main(args)
--- train.py
+++ train.py
@@ -1,19 +1,40 @@
+"""
+Обучение модели для генерации текста на основоне других текстов.
+Используйте -h для вызова справки.
+"""
 import argparse
 import os
 import re
-#import pymorphy2
-
+from collections import Counter
+import pickle
 
 r_alphabet = re.compile('[а-яА-Я0-9-]+|[.,:;!?]+')
 
 
-def gen_token(lines):
-    for line in lines:
-        for token in r_alphabet.findall(line):
-            yield token
+def gen_token(files):
+    """
+    Генератор слов из файлов текстов
+    :param files: Список путей к файлам
+    :type files: list
+    :return: Слово из файла
+    :rtype: str
+    """
+    for file in files:
+        for line in file:
+            for token in r_alphabet.findall(line):
+                yield token
 
 
 def gen_lines(file, lc):
+    """
+    Генератор строк из файла
+    :param file: Путь к файлу
+    :param lc: Флаг на использование lower
+    :type file: str
+    :type lc: bool
+    :return: Строку из файла
+    :rtype: str
+    """
     with open(file, 'r') as f:
         for line in f:
             if lc:
@@ -22,7 +43,33 @@ def gen_lines(file, lc):
                 yield line
 
 
+def open_files(dest_file, lc):
+    """
+    Созданиие списка строк
+    :param dest_file: Путь к файлу
+    :param lc: Флаг на использование lower
+    :type dest_file: str
+    :type lc: bool
+    :return: Список файлов, т.е. список строк
+    :rtype: list
+    """
+    if dest_file is None:
+        #Один input - одна строка в файле
+        return [[input()]]
+    else:
+        return [gen_lines(os.path.join(dest_file, file), lc) for file in os.listdir(dest_file) if file[-4:] == '.txt']
+
+
 def gen_ngrams(token, n):
+    """
+    Генерация n-грамм
+    :param token: Генератор токена
+    :param n: Количество слов в n-грамме
+    :type token: generator
+    :type n: int
+    :return: Список из слов
+    :rtype: list
+    """
     t = ['$' for _ in range(n - 1)]
     for ti in token:
         yield t + [ti]
@@ -35,18 +82,92 @@ def gen_ngrams(token, n):
             t = t[1:] + [ti]
 
 
-def first_step(args):
-    lines = []
-    if args.get('input_dir') is None:
-        lines = input().split()
-    else:
-        for file in os.listdir(args['input_dir']):
-            if file[-4:] == '.txt':
-                lines = gen_lines(os.path.join(args['input_dir'], file), args.get('lc'))
+def gen_model(ngrams):
+    """
+    Генерация модели
+    :param ngrams: Генератор n-грамм
+    :type ngrams: generator
+    :return: Словарь из n-граммы в частоту распределения
+    """
+    p_model = Counter([tuple(i) for i in ngrams])
+    return {i: p_model[i] for i in p_model}
 
-    token = gen_token(lines)
-    bigrams = gen_ngrams(token, args['ngrams'])
-    print('\n'.join([' '.join(i) for i in bigrams]))
+
+def update_model(model, ngrams):
+    """
+    Обновление модели
+    :param model: Модель
+    :param ngrams: Генератор n-грамм
+    :type model: dict
+    :type ngrams: generator
+    :return: Обновленная модель
+    :rtype: dict
+    """
+    u_model = Counter(tuple(ngrams))
+    for i in u_model:
+        if i in model:
+            model[i] += u_model[i]
+        else:
+            model[i] = u_model[i]
+
+    return model
+
+
+def load_model(model_dest):
+    """
+    Загрузка модели из файла
+    :param model_dest: Расположение файла
+    :type: str
+    :return: Модель
+    :rtype: dict
+    """
+    with open(model_dest, 'rb') as f:
+        return pickle.load(f)
+
+
+def save_model(model, model_dest):
+    """
+    Сохранение модели в файл
+    :param model: Модель
+    :param model_dest: Расположение файла
+    :type model: dict
+    :type model_dest: str
+    :return: None
+    """
+    with open(model_dest, 'wb') as f:
+        pickle.dump(model, f)
+
+
+def train_model(ngrams, model_dest):
+    """
+    Главная функция обучения модели
+    :param ngrams: Генератор n-грамм
+    :param model_dest: Расположение файла модели
+    :type ngrams: generator
+    :type model_dest: str
+    :return: None
+    """
+    if model_dest is None:
+        model = gen_model(ngrams)
+        model_dest = input("Write destination for model file:\n")
+        save_model(model, model_dest)
+    else:
+        model = load_model(model_dest)
+        model = update_model(model, ngrams)
+        save_model(model, model_dest)
+
+
+def main(args):
+    """
+    Главная функция
+    :param args: Аргументы запуска обучения
+    :type args: dict
+    :return: None
+    """
+    files = open_files(args['input_dir'], args['lc'])
+    token = gen_token(files)
+    ngrams = gen_ngrams(token, args['ngrams'])
+    train_model(ngrams, args['model'])
 
 
 if __name__ == "__main__":
@@ -54,7 +175,7 @@ if __name__ == "__main__":
     parser.add_argument('--lc', action='store_true', help='apply lower_case() to texts')
     parser.add_argument('--input-dir', action='store',
                         help='destination to texts. If it isn\'t set, uses texts from stdin')
-    parser.add_argument('model', action='store', help='destination to model file')
+    parser.add_argument('--model', action='store', help='destination to model file')
     parser.add_argument('--ngrams', action='store', default=2, type=int, help='number of using words in one token')
     args = vars(parser.parse_args())
-    first_step(args)
+    main(args)
